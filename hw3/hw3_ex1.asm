4ca9454cc7f74ac2868a77d76e41b6337ba3d52f
Kalen Bagano <knbagano@hawaii.edu>
2016-02-12 02:06:19 -1000

    exercise 1 works!!!!

--- hw3/hw3_ex1.asm
+++ hw3/hw3_ex1.asm
@@ -6,7 +6,7 @@ segment .data
 	enterCharacter db "Enter a character: ",0
 	enterInteger db "Enter a integer between 1 and 10: ",0
         characterWas db "The character entered was: ",0
-	transformedCharacter db "The transformed character is : ",0
+	transformedCharacter db "The transformed character is: ",0
 
 segment .bss
 ;
@@ -25,17 +25,21 @@ asm_main:
 	call print_string 	; print "Enter a character: "
 	call read_char		; reads the char inputted by user
 	mov [character], eax	; saves the char inputted by the user into the variable 'character'
-	call print_nl		; prints a new line
 	mov eax, enterInteger	; print "Enter a integer between 1 and 10: "
 	call print_string	; print "Enter a integer between 1 and 10: "
 	call read_int		; reads the integer inputted by the user
 	mov [integer], eax	; saves the integer inputted by the user into the variable 'integer'
-	call print_nl		; prints a new line
 	mov eax, characterWas	; print "The character entered was: "
 	call print_string	; print "The character entered was: "
 	mov eax, [character]	; move the character the user gave into eax
 	call print_char		; print the character the user entered
 	call print_nl		; prints a new line
+	mov eax, transformedCharacter	; print "The transformed character is: "
+	call print_string	; print "The transformed character is: "
+	mov eax, [character]	; transform character
+	add eax, [integer]	; transform character
+	call print_char		; print the transformed character
+	call print_nl
 	popa
 	mov	eax, 0		; return back to C
 	leave
