675dbd5a8dc6827208dd3351045d7e0b424e1d80
Kalen Bagano <knbagano@hawaii.edu>
2016-03-31 02:46:17 -1000

    final hw6_ex1.asm file
    File works perfectly in terms of the requirements for the assignment.

--- hw6/hw6_ex1.asm
+++ hw6/hw6_ex1.asm
@@ -6,8 +6,6 @@ segment .data
 
 	enterBinary db "Enter a (<16 bits) binary number: ", 0
 	decimalValue db "Its decimal value is: ", 0
-	itsOne db "character read was a 1", 0
-	itsZero db "character read was a 0", 0
 
 
 
@@ -32,33 +30,26 @@ main_loop:
 	je end			; jump to end if a new line character is read
 	cmp ecx, 16		; check to see if 16 bits have been read
 	je end			; jump to end if 16 bits have been read
-	shl bx, 1		;
-	cmp eax, 48		;
-	je zero			;
-	jmp one			;
+	shl bx, 1		; shift the register holding the bits to the left by one (this is irrelevant on the first iteration of loop, bits are all zeroes)
+	cmp eax, 48		; check to see if character read was a 0
+	je zero			; jump if character read was a 0
+	jmp one			; jump if character read was not zero (assuming 1 in this case)
 zero:
-	mov eax, itsZero
-	call print_string
-	call print_nl
 	mov dx, 65534		; set dx to a mask with a zero in the rightmost bit
-	and bx, dx		;
+	and bx, dx		; set rightmost bit of bx to zero
 	inc ecx			; increment the number of chars read
-	jmp main_loop		;
+	jmp main_loop		; jump to main loop
 one:
-	mov eax, itsOne
-	call print_string
-	call print_nl
 	mov dx, 1		; set dx to a mask with a 1 in the rightmost bit, and zeros for the rest
-	or bx, dx		;
+	or bx, dx		; set rightmost bit of bx to one
 	inc ecx			; increment the number of chars read
-	jmp main_loop		;
+	jmp main_loop		; jump to main loop
 end:
 	mov eax, decimalValue	; print "Its decimal value is: "
 	call print_string	; print "Its decimal value is: "
-	movzx eax, bx		; print decimal value of number
+	movsx eax, bx		; print decimal value of number
 	call print_int		; print decimal value of number
 	call print_nl		; print a new line
-	
 	popa
 	mov	eax, 0		; return back to C
 	leave
